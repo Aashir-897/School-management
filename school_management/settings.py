54b289fe690d9d35fc4374ba0d3a66f7b9496cf6
Aashir Ahmed <aashirahmed510@gmail.com>
2024-11-06 18:26:33 +0500

    Updated project

--- school_management/settings.py
+++ school_management/settings.py
@@ -111,4 +111,11 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
+import os
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Ensure this path is correct
+    }
+}
 
